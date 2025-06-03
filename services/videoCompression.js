95b2de52af0a810ee5882543e85f5c52e78033c6
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2025-06-03 18:32:43 +0530

    Fix: 240p vid convert error

--- services/videoCompression.js
+++ services/videoCompression.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const path = require('path');
 
 const compressVideo = (inputPath, outputPath, resolution) => {
-console.log("input",inputPath," ",outputPath," ",resolution)
+    console.log("input", inputPath, " ", outputPath, " ", resolution);
     return new Promise((resolve, reject) => {
-        const cmd = `ffmpeg -i "${inputPath}" -vcodec libx264 -crf 28 -preset slow -vf "scale=-1:${resolution}" "${outputPath}"`;
+        const cmd = `ffmpeg -i "${inputPath}" -vcodec libx264 -crf 28 -preset slow -vf "scale='trunc(iw*${resolution}/ih/2)*2':${resolution}" -c:a aac -b:a 128k "${outputPath}"`;
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 console.error('Error during video compression:', error.message);
+                console.error(stderr);
                 reject(error);
                 return;
             }
@@ -18,4 +19,5 @@ console.log("input",inputPath," ",outputPath," ",resolution)
         });
     });
 };
+
 module.exports=compressVideo;
